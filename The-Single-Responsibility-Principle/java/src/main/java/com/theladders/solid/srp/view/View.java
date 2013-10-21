eac581956da8ae7ccf5ee4473b0eddb18fc72fc7
rubychilds <ruby.childs1@gmail.com>
2013-10-21 17:47:47 -0400

    removed all class views into single View class

--- The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/view/View.java
+++ The-Single-Responsibility-Principle/java/src/main/java/com/theladders/solid/srp/view/View.java
@@ -1,11 +1,38 @@
 package com.theladders.solid.srp.view;
 
+import java.util.ArrayList;
 import java.util.Map;
 
 import com.theladders.solid.srp.Result;
 
-public interface View
-{
-  public abstract Result viewResult(Map<String, Object> model);
+public class View{
+  
+  private ArrayList<String> errList = new ArrayList<String>();
+  
+  
+  public Result provideInvalidJobView(Map<String, Object> model)
+  {
+      return new Result("invalidJob", model);  
+  }
+
+  public Result provideResumeCompletionView(Map<String, Object> model)
+  {
+    return new Result("completeResumePlease", model);
+  }
+  
+  public Result provideErrorView(Map<String, Object> model)
+  {
+    return new Result("error", model, errList);
+  }
+
+  public Result provideSuccessView(Map<String, Object> model)
+  {
+    return new Result("success", model);
+  }
+  
+  public void addError(String errorMessage)
+  {
+    errList.add(errorMessage);
+  }
 
 }
