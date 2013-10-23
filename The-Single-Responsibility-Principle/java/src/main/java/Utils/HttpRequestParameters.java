8afa3a796cb0bb5c440bb875e1cba2cbc319773e
rubychilds <ruby.childs1@gmail.com>
2013-10-23 13:57:29 -0400

    made class name more relevant

--- The-Single-Responsibility-Principle/java/src/main/java/Utils/HttpRequestParameters.java
+++ The-Single-Responsibility-Principle/java/src/main/java/Utils/HttpRequestParameters.java
@@ -1,10 +1,11 @@
 package Utils;
 
-public class ParameterNames
+public class HttpRequestParameters
 {
   // need to separate out
   public static String EXISTING = "existing";
   public static String ACTIVATE_RESUME = "yes";
+  
   public static String JOB_ID = "jobId";
   
   public static String WHICH = "whichResume";
