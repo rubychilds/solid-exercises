7f1aace135a2d0f111eb4585afd7854048de5d34
Ruby Childs <rchilds@Administrators-MacBook-Pro-64.local>
2013-11-03 13:26:42 -0500

    added isp

---
+++ The-Interface-Segregation-Principle/java/src/main/java/com/theladders/solid/isp/viewjob/JobRepresentation.java
@@ -0,0 +1,7 @@
+package com.theladders.solid.isp.viewjob;
+
+public class JobRepresentation
+{
+  // This is a stub class that would normally contain fields
+  // that would be serialized into JSON and consumed by a client.
+}
