f7af46a98840064aed4e22558a209a8878828846
Ruby Childs <rchilds@Administrators-MacBook-Pro-64.local>
2013-11-03 19:25:57 -0500

    removed unneeded class ApplicationHandler

---
+++ The-Single-Responsibility-Principle/java/src/test/java/com/theladders/solid/srp/TestApplyProcess.java
@@ -0,0 +1,132 @@
+package com.theladders.solid.srp;
+
+import org.junit.Before;
+
+import com.theladders.solid.srp.job.Job;
+import com.theladders.solid.srp.job.JobRepository;
+import com.theladders.solid.srp.job.JobSearchService;
+import com.theladders.solid.srp.job.application.JobApplicationRepository;
+import com.theladders.solid.srp.job.application.JobApplicationSystem;
+import com.theladders.solid.srp.job.application.SuccessfulApplication;
+import com.theladders.solid.srp.jobseeker.Jobseeker;
+import com.theladders.solid.srp.jobseeker.JobseekerProfile;
+import com.theladders.solid.srp.jobseeker.JobseekerProfileManager;
+import com.theladders.solid.srp.jobseeker.JobseekerProfileRepository;
+import com.theladders.solid.srp.jobseeker.ProfileStatus;
+import com.theladders.solid.srp.resume.ActiveResumeRepository;
+import com.theladders.solid.srp.resume.MyResumeManager;
+import com.theladders.solid.srp.resume.Resume;
+import com.theladders.solid.srp.resume.ResumeManager;
+import com.theladders.solid.srp.resume.ResumeRepository;
+
+public class TestApplyProcess
+{
+
+  private static final int           INVALID_JOB_ID        = 555;
+  private static final String        SHARED_RESUME_NAME    = "A Resume";
+  private static final int           JOBSEEKER_WITH_RESUME = 777;
+  private static final int           INCOMPLETE_JOBSEEKER  = 888;
+  private static final int           APPROVED_JOBSEEKER    = 1010;
+
+  private ApplicationProcess         applicationProcess;
+  private JobRepository              jobRepository;
+  private ResumeRepository           resumeRepository;
+  private JobApplicationRepository   jobApplicationRepository;
+  private JobseekerProfileRepository jobseekerProfileRepository;
+  private ActiveResumeRepository     activeResumeRepository;
+
+  private SuccessfulApplication      existingApplication;
+
+  @Before
+  public void setup()
+  {
+    setupJobseekerProfileRepository();
+    setupJobRepository();
+    setupResumeRepository();
+    setupActiveResumeRepository();
+    setupJobApplicationRepository();
+    setupApplicationProcess();
+  }
+
+  private void setupJobseekerProfileRepository()
+  {
+    jobseekerProfileRepository = new JobseekerProfileRepository();
+
+    addToJobseekerProfileRepository(APPROVED_JOBSEEKER, ProfileStatus.APPROVED);
+    addToJobseekerProfileRepository(INCOMPLETE_JOBSEEKER, ProfileStatus.INCOMPLETE);
+    addToJobseekerProfileRepository(JOBSEEKER_WITH_RESUME, ProfileStatus.APPROVED);
+  }
+
+  private void addToJobseekerProfileRepository(int id,
+                                               ProfileStatus status)
+  {
+    JobseekerProfile profile = new JobseekerProfile(id, status);
+    jobseekerProfileRepository.addProfile(profile);
+  }
+
+  private void setupJobRepository()
+  {
+    jobRepository = new JobRepository();
+
+    addJobToRepository(5);
+    addJobToRepository(15);
+    addJobToRepository(51);
+    addJobToRepository(57);
+    addJobToRepository(501);
+    addJobToRepository(1555);
+    addJobToRepository(5012);
+    addJobToRepository(50111);
+  }
+
+  private void addJobToRepository(int jobId)
+  {
+    if (jobId != INVALID_JOB_ID)
+    {
+      jobRepository.addJob(new Job(jobId));
+    }
+  }
+
+  private void setupResumeRepository()
+  {
+    resumeRepository = new ResumeRepository();
+  }
+
+  private void setupActiveResumeRepository()
+  {
+    activeResumeRepository = new ActiveResumeRepository();
+
+    activeResumeRepository.makeActive(JOBSEEKER_WITH_RESUME, new Resume("Blammo"));
+  }
+
+  private void setupJobApplicationRepository()
+  {
+    jobApplicationRepository = new JobApplicationRepository();
+
+    addToJobApplicationRepository();
+  }
+
+  private void addToJobApplicationRepository()
+  {
+    Jobseeker JOBSEEKER = new Jobseeker(APPROVED_JOBSEEKER, true);
+    Job job = new Job(15);
+    Resume resume = new Resume("foo");
+
+    existingApplication = new SuccessfulApplication(JOBSEEKER, job, resume);
+
+    jobApplicationRepository.add(existingApplication);
+  }
+
+  public void setupApplicationProcess()
+  {
+    JobseekerProfileManager jobseekerProfileManager = new JobseekerProfileManager(jobseekerProfileRepository);
+    JobSearchService jobSearchService = new JobSearchService(jobRepository);
+    JobApplicationSystem jobApplicationSystem = new JobApplicationSystem(jobApplicationRepository);
+    ResumeManager resumeManager = new ResumeManager(resumeRepository);
+    MyResumeManager myResumeManager = new MyResumeManager(activeResumeRepository);
+
+    applicationProcess = new ApplicationProcess(jobseekerProfileManager,
+                                                jobApplicationSystem,
+                                                resumeManager,
+                                                myResumeManager);
+  }
+}
